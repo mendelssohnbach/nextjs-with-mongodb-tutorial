cbc731743f87db28e80d35c735d8af96baefb778
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-11 14:25:00 +0900

    getStaticPropsを実装

---
+++ pages/api/movies.js
@@ -0,0 +1,9 @@
+import { connectToDatabase } from '../../util/mongodb';
+
+export default async (req, res) => {
+  const { db } = await connectToDatabase();
+
+  const movies = await db.collection('movies').find({}).sort({ metacritic: -1 }).limit(5).toArray();
+
+  res.json(movies);
+};
